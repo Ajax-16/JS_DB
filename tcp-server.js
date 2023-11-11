4de1016c3f5ad0aba99eb0162b5bc05fa0343367
Ajax-16 <davidbernardezluque7618@gmail.com>
2023-11-11 23:21:30 +0100

    respuestas del servidor actualizadas

--- tcp-server.js
+++ tcp-server.js
@@ -2,19 +2,35 @@ import net from 'net';
 import fs from 'fs';
 
 const server = net.createServer((socket) => {
-    socket.on('data', (data) => {
-        const dataString = data.toString();
-        const [filePath, fileContent] = dataString.split('|');
-          fs.writeFile(filePath, fileContent, (err) => {
-            if (err) throw err;
-            const oldConsoleLog = console.log;
-            console.log = function (message) {
-            oldConsoleLog.apply(console, arguments);
-            socket.write(message);
-  };
-          });
+  socket.on('data', (data) => {
+    const dataString = data.toString();
+    const [filePath, fileContent] = dataString.split('|');
 
-      });
+    fs.writeFile(filePath, fileContent, (err) => {
+      if (err) {
+        console.error('Error al escribir en el archivo:', err);
+        return;
+      }
+
+      // Después de escribir en el archivo, redirigir la consola al cliente
+      const oldConsoleLog = console.log;
+      console.log = function (message) {
+        oldConsoleLog.apply(console, arguments);
+        socket.write(`mensaje|${message}`);
+      };
+    });
+  });
+
+  socket.on('end', () => {
+    console.log('Cliente desconectado.');
+
+    // Restaurar console.log original cuando el cliente se desconecta
+    console.log = oldConsoleLog;
+  });
+
+  socket.on('error', (err) => {
+    console.error('Error en la conexión del cliente:', err);
+  });
 });
 
 server.listen(3000, '0.0.0.0', () => {
