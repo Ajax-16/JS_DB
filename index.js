deb1eb4db1ea287b32b00b049f9e9ff7a5e9fe11
Ajax-16 <davidbernardezluque7618@gmail.com>
2023-11-11 20:01:56 +0100

    example added on index.js

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import { DB } from "./db.js";
 
 async function main(){
 
-    // let db = new DB('db');
+    let db = new DB('db');
 
-    // await db.init();
+    await db.init();
+
+    await db.createTable('test', 'id', ['name', 'desc'])
+
 }
 
 main();
