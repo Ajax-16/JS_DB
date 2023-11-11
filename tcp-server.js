5e2a1f04b17965c65ac709c286bf940a1a1e8b39
Ajax-16 <davidbernardezluque7618@gmail.com>
2023-11-11 23:36:41 +0100

    respuestas del servidor actualizadas

--- tcp-server.js
+++ tcp-server.js
@@ -1,6 +1,8 @@
 import net from 'net';
 import fs from 'fs';
 
+let oldConsoleLog; // Declarar fuera para que sea accesible en todo el alcance
+
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const dataString = data.toString();
@@ -13,7 +15,7 @@ const server = net.createServer((socket) => {
       }
 
       // Después de escribir en el archivo, redirigir la consola al cliente
-      const oldConsoleLog = console.log;
+      oldConsoleLog = console.log; // Asignar aquí para que esté definido
       console.log = function (message) {
         oldConsoleLog.apply(console, arguments);
         socket.write(`mensaje|${message}`);
@@ -25,7 +27,9 @@ const server = net.createServer((socket) => {
     console.log('Cliente desconectado.');
 
     // Restaurar console.log original cuando el cliente se desconecta
-    console.log = oldConsoleLog;
+    if (oldConsoleLog) {
+      console.log = oldConsoleLog;
+    }
   });
 
   socket.on('error', (err) => {
