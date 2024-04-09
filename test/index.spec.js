adc4e2384805c8d1484c3cb779bc6ccea32651d1
Ajax-16 <davidbernardezluque7618@gmail.com>
2024-04-09 14:16:57 +0200

    solved mkdir problem?

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,9 +2,15 @@ import { DB } from "../index.js"
 import jest from "jest-mock";
 import { promises as fs } from 'fs';
 
-const testDBInstance = new DB("test");
+let testDBInstance;
 
 beforeEach(() => {
+
+    const mkdirMock = jest.fn().mockReturnValue(true);
+    fs.mkdir = mkdirMock;
+
+    testDBInstance = new DB("test");
+
     const testDBText =
     [
         [
@@ -22,6 +28,10 @@ beforeEach(() => {
             ]
         ]
     ]
+
+    const saveMock = jest.fn().mockReturnValue(true);
+    testDBInstance.save = saveMock;
+
     Object.defineProperty(testDBInstance, 'tables', {
         value: testDBText,
         writable: true
@@ -31,86 +41,76 @@ beforeEach(() => {
         writable: true
     });
 
-    const saveMock = jest.fn().mockReturnValue(true);
-
-    testDBInstance.save = saveMock;
 })
 
-describe("Test", ()=>{
-    test("test 1", ()=>{
-        const test = true;
-        expect(test).toBe(true)
+describe("Create table test", () => {
+    test("Should not create a table because there are duplicated normal columns", async () => {
+        const tableCreate = await testDBInstance.createTable({ tableName: "test_table_2", primaryKey: "id", columns: ["test_column_1", "test_column_1"] })
+        expect(tableCreate).toBe(false);
+    })
+    test("Should not create a table because there are duplicated normal and fk columns", async () => {
+        const tableCreate = await testDBInstance.createTable({
+            tableName: "test_table_2", primaryKey: "id", columns: ["test_column_1", "test_column_2"], foreignKeys: [{
+                name: "test_fk", columnName: "test_column_1", referenceTable: "test_table_2", referenceColumn: "test_column_2"
+            }]
+        })
+        expect(tableCreate).toBe(false);
+    })
+    test("Should not create a table because there are duplicated fk columns", async () => {
+        const tableCreate = await testDBInstance.createTable({
+            tableName: "test_table_2", primaryKey: "id", foreignKeys: [
+                {
+                    name: "test_fk", columnName: "test_column_1", referenceTable: "test_table_2", referenceColumn: "test_column_2"
+                },
+                {
+                    name: "test_fk", columnName: "test_column_1", referenceTable: "test_table_3", referenceColumn: "test_column_3"
+                }
+            ]
+        })
+        expect(tableCreate).toBe(false);
+    })
+    test("Should not create a table because the name is in use", async () => {
+        const tableCreate = await testDBInstance.createTable({ tableName: "test_table_1", primaryKey: "id", columns: ["test_column_1", "test_column_2"] })
+        expect(tableCreate[0][0]).toEqual("EXCEPTION ENCOUNTER");
+    })
+    test("Should not create a table because the name is a keyword", async () => {
+        const tableCreate = await testDBInstance.createTable({ tableName: "EXCEPTION ENCOUNTER", primaryKey: "id", columns: ["test_column_1", "test_column_2"] })
+        expect(tableCreate[0][0]).toEqual("EXCEPTION ENCOUNTER");
+    })
+    test("Should create a table with any conflicts", async () => {
+        const tableCreate = await testDBInstance.createTable({ tableName: "test_table_2", primaryKey: "id", columns: ["test_column_1", "test_column_2"] })
+        expect(tableCreate).toBe(true);
     })
 })
 
-// describe("Create table test", () => {
-//     test("Should not create a table because there are duplicated normal columns", async () => {
-//         const tableCreate = await testDBInstance.createTable({ tableName: "test_table_2", primaryKey: "id", columns: ["test_column_1", "test_column_1"] })
-//         expect(tableCreate).toBe(false);
-//     })
-//     test("Should not create a table because there are duplicated normal and fk columns", async () => {
-//         const tableCreate = await testDBInstance.createTable({
-//             tableName: "test_table_2", primaryKey: "id", columns: ["test_column_1", "test_column_2"], foreignKeys: [{
-//                 name: "test_fk", columnName: "test_column_1", referenceTable: "test_table_2", referenceColumn: "test_column_2"
-//             }]
-//         })
-//         expect(tableCreate).toBe(false);
-//     })
-//     test("Should not create a table because there are duplicated fk columns", async () => {
-//         const tableCreate = await testDBInstance.createTable({
-//             tableName: "test_table_2", primaryKey: "id", foreignKeys: [
-//                 {
-//                     name: "test_fk", columnName: "test_column_1", referenceTable: "test_table_2", referenceColumn: "test_column_2"
-//                 },
-//                 {
-//                     name: "test_fk", columnName: "test_column_1", referenceTable: "test_table_3", referenceColumn: "test_column_3"
-//                 }
-//             ]
-//         })
-//         expect(tableCreate).toBe(false);
-//     })
-//     test("Should not create a table because the name is in use", async () => {
-//         const tableCreate = await testDBInstance.createTable({ tableName: "test_table_1", primaryKey: "id", columns: ["test_column_1", "test_column_2"] })
-//         expect(tableCreate[0][0]).toEqual("EXCEPTION ENCOUNTER");
-//     })
-//     test("Should not create a table because the name is a keyword", async () => {
-//         const tableCreate = await testDBInstance.createTable({ tableName: "EXCEPTION ENCOUNTER", primaryKey: "id", columns: ["test_column_1", "test_column_2"] })
-//         expect(tableCreate[0][0]).toEqual("EXCEPTION ENCOUNTER");
-//     })
-//     test("Should create a table with any conflicts", async () => {
-//         const tableCreate = await testDBInstance.createTable({ tableName: "test_table_2", primaryKey: "id", columns: ["test_column_1", "test_column_2"] })
-//         expect(tableCreate).toBe(true);
-//     })
-// })
+describe("Insert on table test", () => {
+    test("Should insert on table", async ()=>{
+        const tableInsert = await testDBInstance.insert({tableName: "test_table_1", values: ["test_value_1", "test_value_2", "test_value_3"]});
+        expect(tableInsert).toBe(true)
+    })
+    test("Should not insert on table because table doesn\'t exist", async ()=>{
+        const tableInsert = await testDBInstance.insert({tableName: "table_test_1", values: ["test_value_1", "test_value_2", "test_value_3"]});
+        expect(tableInsert[0][0]).toEqual("EXCEPTION ENCOUNTER");
+    })
+})
 
-// describe("Insert on table test", () => {
-//     test("Should insert on table", async ()=>{
-//         const tableInsert = await testDBInstance.insert({tableName: "test_table_1", values: ["test_value_1", "test_value_2", "test_value_3"]});
-//         expect(tableInsert).toBe(true)
-//     })
-//     test("Should not insert on table because table doesn\'t exist", async ()=>{
-//         const tableInsert = await testDBInstance.insert({tableName: "table_test_1", values: ["test_value_1", "test_value_2", "test_value_3"]});
-//         expect(tableInsert[0][0]).toEqual("EXCEPTION ENCOUNTER");
-//     })
-// })
-
-// describe("Delete one element from table test", ()=>{
-//     test("Should delete on table", async ()=>{
-//         // test element
-//         await testDBInstance.insert({tableName: "test_table_1", values: ["test_value_1", "test_value_2", "test_value_3"]});
-//         const tableDelete = await testDBInstance.delete({tableName: "test_table_1", condition: "id", operator: "=", conditionValue: 0})
-//         expect(tableDelete).toBe(true);
-//     })
-//     test("Should not delete on table because the element doesn\'t exist", async ()=>{
-//         const tableDelete = await testDBInstance.delete({tableName: "test_table_1", condition: "id", operator: "=", conditionValue: 1})
-//         expect(tableDelete).toBe(false);
-//     })
-//     test("Should not delete on table because a column is invalid", async ()=>{
-//         const tableDelete = await testDBInstance.delete({tableName: "test_table_1", condition: "invented_column", operator: "=", conditionValue: "value"})
-//         expect(tableDelete[0][0]).toEqual("EXCEPTION ENCOUNTER");
-//     })
-//     test("Should not delete on table because the table doesn\'t exist", async ()=>{
-//         const tableDelete = await testDBInstance.delete({tableName: "invented_table", condition: "id", operator: "=", conditionValue: 0})
-//         expect(tableDelete[0][0]).toEqual("EXCEPTION ENCOUNTER");
-//     })
-// })
+describe("Delete one element from table test", ()=>{
+    test("Should delete on table", async ()=>{
+        // test element
+        await testDBInstance.insert({tableName: "test_table_1", values: ["test_value_1", "test_value_2", "test_value_3"]});
+        const tableDelete = await testDBInstance.delete({tableName: "test_table_1", condition: "id", operator: "=", conditionValue: 0})
+        expect(tableDelete).toBe(true);
+    })
+    test("Should not delete on table because the element doesn\'t exist", async ()=>{
+        const tableDelete = await testDBInstance.delete({tableName: "test_table_1", condition: "id", operator: "=", conditionValue: 1})
+        expect(tableDelete).toBe(false);
+    })
+    test("Should not delete on table because a column is invalid", async ()=>{
+        const tableDelete = await testDBInstance.delete({tableName: "test_table_1", condition: "invented_column", operator: "=", conditionValue: "value"})
+        expect(tableDelete[0][0]).toEqual("EXCEPTION ENCOUNTER");
+    })
+    test("Should not delete on table because the table doesn\'t exist", async ()=>{
+        const tableDelete = await testDBInstance.delete({tableName: "invented_table", condition: "id", operator: "=", conditionValue: 0})
+        expect(tableDelete[0][0]).toEqual("EXCEPTION ENCOUNTER");
+    })
+})
