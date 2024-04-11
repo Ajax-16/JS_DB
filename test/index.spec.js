e1c5c7241d8918ee7c3812835b3014620f047b4f
Ajax-16 <davidbernardezluque7618@gmail.com>
2024-04-11 14:13:03 +0200

    updated update test

--- test/index.spec.js
+++ test/index.spec.js
@@ -370,6 +370,8 @@ describe("Update element from table test", ()=>{
     test("Should update one column value on table", async ()=>{
         await testDBInstance.insert({ tableName: "test_table_1", values: [`test_value_1`, `test_value_2`, `test_value_3`] });
         const updateTable = await testDBInstance.update({tableName: "test_table_1", set: ["test_column_1"], setValues: ["new_value"], condition: "id", operator: "=", conditionValue: 0})
+        const updatedElementRow = testDBInstance.tables[0][2]
         expect(updateTable).toBe(true);
+        expect(updatedElementRow).toEqual(expect.arrayContaining(["new_value"]))
     })
 })
